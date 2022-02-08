c0adc96d829cba9e4e1e5968fd0034e483dd0a1a
Alvaro <alvaroscasgar@gmail.com>
2022-02-08 13:30:48 +0100

    Login Completo, solo falta el logo

--- frontDual/src/app/shared/navbar/navbar.component.ts
+++ frontDual/src/app/shared/navbar/navbar.component.ts
@@ -8,13 +8,31 @@ import {RouterLink, RouterLinkActive} from "@angular/router";
 })
 export class NavbarComponent implements OnInit {
 
+  nombre:string="Alvaro Castano";
+  iniciales:string=this.inicialesCap(this.nombre);
   constructor() {
-
   }
 
   ngOnInit(): void {
+    console.log(this.iniciales)
   }
 
+// @ts-ignore
+  inicialesCap(nombre:string):string{
+    let iniciales:string
+    let mayus:string=nombre.toUpperCase();
+    iniciales=mayus.slice(0,1);
+  for (let i = 0; i < mayus.length; i++) {
+    if (mayus[i] == ' ') {
+
+      iniciales=iniciales+mayus[i+1];
+
+
+    }
+
+  }
+    return iniciales;
+}
 
 
 }
