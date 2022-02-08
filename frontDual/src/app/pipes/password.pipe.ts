695a1e65ac8c90f4f60bf773ee9301f710a63d8a
Alvaro <alvaroscasgar@gmail.com>
2022-02-08 10:14:31 +0100

    Login Completo

---
+++ frontDual/src/app/pipes/password.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'password'
+})
+export class PasswordPipe implements PipeTransform {
+
+  transform(value: boolean, ...args: unknown[]): unknown {
+    if (value == true) {
+      return 'text';
+    } else {
+      return 'password';
+    }
+  }
+}
