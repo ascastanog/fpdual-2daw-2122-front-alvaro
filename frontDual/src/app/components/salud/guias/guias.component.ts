b99df388244e7dd456ea8fdde33212ecca182ac6
Alvaro <alvaroscasgar@gmail.com>
2022-02-10 21:18:22 +0100

    salud terminado completo

--- frontDual/src/app/components/salud/guias/guias.component.ts
+++ frontDual/src/app/components/salud/guias/guias.component.ts
@@ -11,5 +11,32 @@ export class GuiasComponent implements OnInit {
 
   ngOnInit(): void {
   }
+guia1:Guia= new Guia( "./assets/pexels-karolina-grabowska-4021769.jpg", "Trabajo con manipulacion de cargas", ["Espalda", "Teletrabajo"]);
 
+
+  guia2:Guia= new Guia( "./assets/pexels-kindel-media-7298881.jpg", "Columna cervical", ["Espalda"]);
+
+  guia3:Guia= new Guia( "./assets/pexels-karolina-grabowska-4506206.jpg", "Columna dorsal", ["Espalda", "Teletrabajo"]);
+
+  guia4:Guia= new Guia( "./assets/pexels-karolina-grabowska-4506216.jpg", "Columna lumbar", [ "Teletrabajo"]);
+
+  guia5:Guia= new Guia( "./assets/pexels-cottonbro-6191918.jpg", "Como mantener una postura de espalda adecuada en el trabajo", ["Espalda", "Teletrabajo"]);
+
+  guia6:Guia= new Guia( "./assets/pexels-darya-gryazeva-11072353.jpg", "Problemas venosos en piernas", ["Espalda", "Teletrabajo"]);
+
+  guia7:Guia= new Guia( "./assets/pexels-sora-shimazaki-5938558.jpg", "Trabajo con manipulacion de cargas", ["Espalda", "Teletrabajo"]);
+
+  guias:Guia[]=[this.guia1, this.guia2, this.guia3, this.guia4, this.guia5, this.guia6, this.guia7 ];
+}
+
+class Guia{
+  src:string;
+  titulo:string;
+  campos:string[];
+
+  constructor(src: string, titulo: string, campos: string[]) {
+    this.src = src;
+    this.titulo = titulo;
+    this.campos = campos;
+  }
 }
